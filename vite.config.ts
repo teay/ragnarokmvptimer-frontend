a1d11d4ea4b9681810880dfa1f182ce9e75db23c
teay <teay20000@gmail.com>
2025-03-17 02:55:22 +0700

    some config from ragnarokmvptimer to this repo

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import wyw from '@wyw-in-js/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ragnarokmvptimer/',
   json: {
     stringify: true,
   },
@@ -23,6 +24,7 @@ export default defineConfig({
     react(),
     wyw({
       include: ['**/*.{ts,tsx}'],
+      base: '/ragnarokmvptimer/',
     }),
     imagetools({
       exclude: ['./src/assets/mvp_icons_animated/**/*'],
@@ -55,50 +57,51 @@ export default defineConfig({
         ],
         icons: [
           {
-            src: '/icons/favicon-16x16.png',
+            src: '/ragnarokmvptimer/icons/favicon-16x16.png',
             sizes: '16x16',
             type: 'image/png',
           },
           {
-            src: '/icons/favicon-32x32.png',
+            src: '/ragnarokmvptimer/icons/favicon-32x32.png',
             sizes: '32x32',
             type: 'image/png',
           },
           {
-            src: '/icons/android-chrome-192x192.png',
+            src: '/ragnarokmvptimer/icons/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icons/android-chrome-256x256.png',
+            src: '/ragnarokmvptimer/icons/android-chrome-256x256.png',
             sizes: '256x256',
             type: 'image/png',
           },
         ],
         screenshots: [
           {
-            src: '/screenshots/site-screenshot.webp',
+            src: '/ragnarokmvptimer/screenshots/site-screenshot.webp',
             sizes: '1903x1079',
             type: 'image/webp',
             form_factor: 'wide',
             label: 'Main page',
           },
+          // แก้ไข path ของรูปภาพอื่นๆ ด้วยเช่นกัน...
           {
-            src: '/screenshots/desktop-servers-screenshot.webp',
+            src: '/ragnarokmvptimer/screenshots/desktop-servers-screenshot.webp',
             sizes: '1903x1079',
             type: 'image/webp',
             form_factor: 'wide',
             label: 'Server selection',
           },
           {
-            src: '/screenshots/mobile-screenshot.webp',
+            src: '/ragnarokmvptimer/screenshots/mobile-screenshot.webp',
             sizes: '320x736',
             type: 'image/webp',
             form_factor: 'narrow',
             label: 'Main page on mobile',
           },
           {
-            src: '/screenshots/mobile-servers-screenshot.webp',
+            src: '/ragnarokmvptimer/screenshots/mobile-servers-screenshot.webp',
             sizes: '320x736',
             type: 'image/webp',
             form_factor: 'narrow',
